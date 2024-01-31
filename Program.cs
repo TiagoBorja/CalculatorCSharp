c0f5472bfa1150e00efd535a3274b7f975cd8475
Tiago de Borja Reis Rodrigues <158228629+TiagoBorja@users.noreply.github.com>
2024-01-31 15:25:15 +0000

    Add a DeBug directory.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalculatorC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
