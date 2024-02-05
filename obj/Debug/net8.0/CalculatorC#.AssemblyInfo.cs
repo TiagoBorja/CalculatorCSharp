1100a4b742f330c20e1c9a5c40a324b662662e95
Tiago de Borja Reis Rodrigues <158228629+TiagoBorja@users.noreply.github.com>
2024-02-05 19:18:41 +0000

    Required for merge.

--- obj/Debug/net8.0/CalculatorC#.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculatorC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90bd7955e86ab1b4f464ef00a9541120e16b0d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c126dd340518585faced6dbbf4b0fc10bba2213")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
